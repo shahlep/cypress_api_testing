8ea41b67ea29917597d544b1d988f86512357045
Shah rahman <toshah.rahman@gmail.com>
2022-02-26 16:43:43 +0200

    api testing GET

--- cypress/integration/api.spec.js
+++ cypress/integration/api.spec.js
@@ -1,7 +1,9 @@
 /// <reference types="Cypress" />
 
 describe('API testing', () => {
-  it('api testing get', () => {
-      
+  it('api testing GET with status code', () => {
+    cy.request('https://reqres.in/api/users/3').as('chl')
+    cy.get('@chl').its('status').should('equal',200)
+
   })  
 })
