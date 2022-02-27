c1eac672887c2ce1410444b6bbc2b9971f347681
Shah rahman <toshah.rahman@gmail.com>
2022-02-27 13:05:02 +0200

    using fixture to post

--- cypress/integration/api-post.spec.js
+++ cypress/integration/api-post.spec.js
@@ -1,29 +1,53 @@
 /// <reference types="Cypress" />
 
-const userdata = require('../fixtures/users.json')
+//const userdata = require('../fixtures/users.json')
 
 describe('API POST Request', () => {
     let accessToken =''
   it('API post request with json payload', () => {
-      cy.request({
-          method: 'POST',
-          url: 'https://gorest.co.in/public/v2/users',
-          headers:{
-              'authorization':'Bearer '+ accessToken
-          },
-          body:{
-                "name": userdata.name,
-                "email": userdata.email,
-                "gender": userdata.gender,
-                "status": userdata.status
+      //using fixture
+      cy.fixture('createuser').then((payload) =>{
+            
+        //1. create user (POST)
+        cy.request({
+            method: 'POST',
+            url: 'https://gorest.co.in/public/v2/users',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            body: {
+                "name": payload.name,
+                "gender": payload.gender,
+                "email": testEmail,
+                "status":payload.status
             }
 
-      }).then(res=>{
-          expect(res.status).to.eq(201)
-          expect(res.body).has.property('name',userdata.name)
-          expect(res.body).has.property('email',userdata.email)
-          expect(res.body).has.property('gender',userdata.gender)
-          expect(res.body).has.property('status',userdata.status)
-      })
-  })  
+        }).then((res)=>{
+            expect(res.status).to.eq(201)
+            expect(res.body).has.property('email', payload.email)
+            expect(res.body).has.property('name',payload.name)
+            expect(res.body).has.property('status',payload.status)
+            expect(res.body).has.property('gender',payload.gender)
+        }).then((res) =>{
+               const userId = res.body.data.id 
+                cy.log("user id is: " + userId)
+                //2. get user (GET)
+                cy.request({
+                    method: 'GET',
+                    url: 'https://gorest.co.in/public/v2/users/'+userId,
+                    headers: {
+                        'Authorization': 'Bearer ' + accessToken
+                    }
+                }).then((res)=>{
+                    expect(res.status).to.eq(200)
+                    expect(res.body).has.property('id', userId)
+                    expect(res.body).has.property('name',payload.name)
+                    expect(res.body).has.property('status',payload.status)
+                    expect(res.body).has.property('email', payload.email)
+                })
+        })
+        
+    
+    })
+})
 })
