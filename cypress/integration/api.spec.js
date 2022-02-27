910a9cf68cfbe88ce81ef0ca6d263a044e528465
Shah rahman <toshah.rahman@gmail.com>
2022-02-27 09:16:13 +0200

    get requset and response assertion

--- cypress/integration/api.spec.js
+++ cypress/integration/api.spec.js
@@ -1,21 +1,18 @@
 /// <reference types="Cypress" />
 
-describe('API testing', () => {
-  it('api testing GET with status code', () => {
-    cy.request('https://reqres.in/api/users/3').as('chl')
-    cy.get('@chl').its('status').should('equal',200)
-
-  })  
-})
 
 describe('Get API user requsts', () => {
+  let accessToken = '751646e81088eebda18943ad7f27ca864e24fe1fa695fb0652a0eace7308869e'
   it('Get Users', () => {
     cy.request({
       method: 'GET',
       url: 'https://gorest.co.in/public/v2/users',
       headers:{
-        'authorization':'Bearer YOUR_ACCESS_TOKEN'
+        'authorization':'Bearer '+ accessToken
       }
+    }).then((res)=>{
+      expect(res.status).to.eq(200)
+      expect(res.body.meta.pagination.limit).to.eq(20)
     })
     
   })
