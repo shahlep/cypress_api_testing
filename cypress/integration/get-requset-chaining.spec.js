c6e160827a189d140b59dc62eacb4511180c3284
Shah rahman <toshah.rahman@gmail.com>
2022-02-28 17:30:13 +0200

    check all cities,using FOR loop

--- cypress/integration/get-requset-chaining.spec.js
+++ cypress/integration/get-requset-chaining.spec.js
@@ -16,9 +16,37 @@ describe('GET request chaining', () => {
                     url:'https://www.metaweather.com/api/location/search/?query='+city,
                     }).then((res)=>{
                         expect(res.status).to.eq(200)
-                        expect(res.body[0]).has.property('title','London')
+                        expect(res.body[0]).has.property('title',city)
 
                     })
+                })
+    })
+
+    // all 
+
+    it('Weather information for all cities', () => {
+        
+        cy.request({
+            method:'GET',
+            url:'https://www.metaweather.com/api/location/search/?query=na',
+            }).then((res)=>{
+                expect(res.status).to.eq(200)
+                const location=res.body
+                return location
+            }).then((location)=>{
+
+                // using for loop to go through all cities 
+                for (let i=0; i<location.length; i++)
+                {
+                cy.request({
+                    method:'GET',
+                    url:'https://www.metaweather.com/api/location/search/?query='+location[i].title
+                    }).then((res)=>{
+                        expect(res.status).to.eq(200)
+                        expect(res.body[0]).has.property('title',location[i].title)
+
+                    })
+                }
                 })
     })
 })
