a2450f3a36e4b5f0a2e545a12571dd1d7bf367ef
Shah rahman <toshah.rahman@gmail.com>
2022-02-28 07:08:43 +0200

    typo fix

--- cypress/integration/get-requset-chaining.spec.js
+++ cypress/integration/get-requset-chaining.spec.js
@@ -7,7 +7,7 @@ describe('GET request chaining', () => {
             method:'GET',
             url:'https://www.metaweather.com/api/location/search/?query=lon',
             }).then((res)=>{
-                expect(res.status).to.be(200)
+                expect(res.status).to.eq(200)
                 const city=res.body[0].title
                 return city
             }).then((city)=>{
@@ -15,7 +15,7 @@ describe('GET request chaining', () => {
                     method:'GET',
                     url:'https://www.metaweather.com/api/location/search/?query='+city,
                     }).then((res)=>{
-                        expect(res.status).to.be(200)
+                        expect(res.status).to.eq(200)
                         expect(res.body[0]).has.property('title','London')
 
                     })
